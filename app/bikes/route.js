7953ad25b2b62beeecd7f900cdebd29591c8f693
rosmith76 <robbe.smith@gmail.com>
2016-11-07 16:27:48 -0500

    Delete bikes function added Added delete button to bike view state. Since the user is viewing a specific bike, that bike is targeted by the delete button. Once the delete action fires the view state transitions to bikes.

--- app/bikes/route.js
+++ app/bikes/route.js
@@ -4,4 +4,13 @@ export default Ember.Route.extend({
   model () {
 return this.get('store').findAll('bike');
 },
+// actions: {
+//   deleteBike (data) {
+//     let bike = data;
+//     console.log('getting to destroyRecord');
+//     console.log(bike);
+//     bike.destroyRecord();
+//     this.transitionTo('bikes');
+// },
+// },
 });
--- app/components/my-bike/component.js
+++ app/components/my-bike/component.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  delete () {
+  return this.sendAction('delete', this.get('bike'));
+},
+tagName: 'li',
+
+  bike: {},
+  actions: {
+    delete () {
+      console.log('getting to the delete action in my-bike component');
+      console.log(this.get('bike'));
+      this.sendAction('delete', this.get('bike'));
+    },
+  },
 });
