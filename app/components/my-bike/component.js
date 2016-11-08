da9e28392d1e76ee8aae70254ec943aa488fd28c
rosmith76 <robbe.smith@gmail.com>
2016-11-07 21:26:36 -0500

    Delete bike second commit
    I had to make a few small changes to stop an error message. The delete
    function still worked, but I was getting an error message. Action now
    works without any errors.

--- app/components/my-bike/component.js
+++ app/components/my-bike/component.js
@@ -9,8 +9,8 @@ tagName: 'li',
   bike: {},
   actions: {
     delete () {
-      console.log('getting to the delete action in my-bike component');
-      console.log(this.get('bike'));
+      // console.log('getting to the delete action in my-bike component');
+      // console.log(this.get('bike'));
       this.sendAction('delete', this.get('bike'));
     },
   },
