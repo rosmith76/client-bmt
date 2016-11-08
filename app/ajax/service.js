13d69ee1cb852ab95a3db052f104f438eb29d7bc
rosmith76 <robbe.smith@gmail.com>
2016-11-08 12:42:09 -0500

    update file paths

--- app/ajax/service.js
+++ app/ajax/service.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 import AjaxService from 'ember-ajax/services/ajax';
-import ENV from 'murmuring-lake-34682/config/environment';
-// import ENV from 'client-bmt/config/environment';
+// import ENV from 'murmuring-lake-34682/config/environment';
+import ENV from 'client-bmt/config/environment';
 
 export default AjaxService.extend({
   host: ENV.apiHost,
