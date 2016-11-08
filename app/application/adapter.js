18f96324cdcce58af1ffaadab74d5508f9fc1981
rosmith76 <robbe.smith@gmail.com>
2016-11-08 11:21:46 -0500

    Commit of files modified for deployment

--- app/application/adapter.js
+++ app/application/adapter.js
@@ -1,4 +1,5 @@
-import ENV from 'client-bmt/config/environment';
+// import ENV from 'client-bmt/config/environment';
+import ENV from 'murmuring-lake-34682/config/environment';
 import ActiveModelAdapter from 'active-model-adapter';
 
 import Ember from 'ember';
