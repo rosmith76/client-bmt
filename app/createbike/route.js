dc444707b5b171d4029df98c52e9c99d312d0989
rosmith76 <robbe.smith@gmail.com>
2016-11-09 21:17:15 -0500

    Added Clear Form button on create bike form
    There is no way to clear the form that we could find. Others
    are having the same issues, and no one is able to fix it.

--- app/createbike/route.js
+++ app/createbike/route.js
@@ -5,12 +5,14 @@ export default Ember.Route.extend({
 
   actions: {
     createBike (newBike) {
+      console.log("insdie the createBike action in the createbike route");
+
       let bike = this.get('store').createRecord('bike', newBike);
       return bike.save()
-      .then(() => this.transitionTo('bikes'))
-      .catch((err) => {
-        console.error(err);
-      });
+        .then(() => this.transitionTo('bikes'));
+        // .catch((err) => {
+        // console.error(err);
+      // });
     },
   },
 });
